482eef6863fcc83831328e502fb05060ed128f0c
Muhammad Rifqy <hadimrifqyfakhrul@gmail.com>
2021-08-22 06:50:36 +0700

    db migration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,11 +12,49 @@
         <div class="col-md-12 mt-5">
             <div class="card">
                 <div class="card-header">
+                    <a href="{{ route('logout') }}" class="btn btn-danger float-right">Logout</a>
                     <h3>Dashboard</h3>
                 </div>
                 <div class="card-body">
                     <h5>Selamat datang di halaman dashboard, <strong>{{ Auth::user()->name }}</strong></h5>
-                    <a href="{{ route('logout') }}" class="btn btn-danger">Logout</a>
+                    <span>berikut list penjualan kamu :)</span>
+
+                    <div class="mt-3">
+                        
+                        <table class="table table-hover">
+                            <thead>
+                              <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle </th>
+                                <th scope="col" style="width: 5%"><a href="javascript:void(0)" class="btn btn-success">+</a></th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td></td>
+                              </tr>
+                              <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                <td></td>
+                              </tr>
+                              <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td></td>
+                              </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
